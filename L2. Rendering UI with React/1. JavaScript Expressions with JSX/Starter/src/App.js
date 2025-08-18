2576991cea1382dce8b03ce32ea6be8ee5e26576
Nikita Pursani <nikitapursani@gmail.com>
2025-08-18 22:01:25 +0530

    display three things

--- L2. Rendering UI with React/1. JavaScript Expressions with JSX/Starter/src/App.js
+++ L2. Rendering UI with React/1. JavaScript Expressions with JSX/Starter/src/App.js
@@ -1,9 +1,17 @@
 import logo from "./logo192.png";
 import "./App.css";
 
-const App = () => {
+const logoImage = () => {
+  return <img src={logo}></img>
+}
+
+const nameHeading = () => {
   const name = "React";
 
+  return <h1>{name}</h1>
+}
+
+const factsList = () => {
   const facts = {
     releaseDate: "May 2013",
     language: "JavaScript",
@@ -13,7 +21,17 @@ const App = () => {
   const aboutReact = (facts) =>
     `Release date: ${facts.releaseDate}. Language: ${facts.releaseDate}. License: ${facts.license}.`;
 
-  return <div className="container"></div>;
+  return <p>{aboutReact}</p>
+
+}
+ 
+const App = () => {
+
+  return <div className="container">
+    <logoImage />
+    <nameHeading />
+    <factsList />
+  </div>;
 };
 
 export default App;
