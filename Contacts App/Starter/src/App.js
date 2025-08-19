12c42ac0259b11591347a819006f429f06ab16f6
Nikita Pursani <nikitapursani@gmail.com>
2025-08-19 23:28:12 +0530

    contacts app as in video

--- Contacts App/Starter/src/App.js
+++ Contacts App/Starter/src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
+import ContactsList from "./components/ContactsList";
 
 const App = () => {
-    return <div>Hello World</div>;
+
+    const contacts = [
+        {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "@richardkalehoff",
+        avatarURL: "richard.jpg",
+        },
+        {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "@karen_isgrigg",
+        avatarURL: "karen.jpg",
+        },
+        {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "@tylermcginnis",
+        avatarURL: "tyler.jpg",
+        },
+    ];
+
+    return (
+        <div>
+            <ContactsList contacts={contacts}/>
+        </div>
+    );
 };
 
 export default App;
---
+++ Contacts App/Starter/src/components/ContactsList.js
@@ -0,0 +1,25 @@
+const ContactsList = ({ contacts }) => {
+  return (
+    <ol className="contact-list">
+      {contacts.map((contact) => {
+        return (
+          <li key={contact.id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`,
+              }}
+            ></div>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button className="contact-remove"></button>
+          </li>
+        );
+      })}
+    </ol>
+  );
+};
+
+export default ContactsList;
